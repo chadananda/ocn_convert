0cd84c80281f4bbb8b1f3c93fa33f144ed37f060
David Hunt <git@dnotes.net>
2018-09-04 21:04:05 -0700

    Got htmlToMarkdown working

--- src/converters/htmlToMarkdown.js
+++ src/converters/htmlToMarkdown.js
@@ -1,25 +1,98 @@
-const OceanMarkdown = require('./oceanMarkdown')
+const Converter = require('./oceanMarkdown')
 const TurndownService = require('turndown')
+const tables = require('turndown-plugin-gfm').tables
 const cheerio = require('cheerio')
+const { URL } = require('url')
+const fp = require('../tools/filePath')
+const Sema = require('async-sema')
 
-class HtmlToMarkdown extends OceanMarkdown {
+class HtmlToMarkdown extends Converter {
   constructor(input, opts = {}, meta = {}, raw = '') {
     super(input, opts)
-    this.toMd = new TurndownService({headingStyle: 'atx'}).remove('script')
-    this.contentElements = ['body']
-    this.metaElements = {
-      title: 'title',
-      author: '',
+    this.addDefaultConversionOpts({
+      contentElements: ['body'],
+      metaElements: {
+        title: 'title',
+        author: '',
+      },
+    })
+    try {
+      this.url = new URL(opts.sourceUrl || opts._convertedFrom)
+      opts.sourceUrl = this.url.toString()
     }
+    catch(e) {
+      this.url = ''
+    }
+    this.mergeAllOptions(opts)
+    this.$ = cheerio.load(this.raw)
+
+    Object.keys(this.opts.metaElements).forEach(k => {
+      if (this.opts.metaElements[k]) this.meta[k] = this.$(this.opts.metaElements[k]).text()
+    })
+  
+
+    this.toMd = new TurndownService({headingStyle: 'atx', emDelimiter: '*'})
+      .remove('script')
+      .addRule('absoluteLinks', {
+        filter: function (node, options) {
+          return (
+            node.nodeName === 'A' &&
+            node.getAttribute('href')
+          )
+        },
+        replacement: function(content, node, options) {
+          let href = node.getAttribute('href')
+          if (!/^#/.test(href)) {
+            href = new URL(node.getAttribute('href'), this.url).toString()
+          }
+          let title = node.title ? ' "' + node.title + '"' : ''
+          return '[' + content + '](' + href + title + ')'
+        }.bind(this)
+      })
+      .addRule('absoluteImages', {
+        filter: 'img',
+        replacement: function (content, node) {
+          var alt = node.alt || ''
+          var src = new URL(node.getAttribute('src'), this.url)
+          var title = node.title || ''
+          var titlePart = title ? ' "' + title + '"' : ''
+          return src ? '![' + alt + ']' + '(' + src + titlePart + ')' : ''
+        }.bind(this)
+      })
+
+    this.subLinks = []
+    this.subTexts = []
+    
+    this.prepareContent()
+
   }
 }
 
-HtmlToMarkdown.prototype.convert = function() {
-  const $ = cheerio.load(this.raw)
-  Object.keys(this.metaElements).forEach(k => {
-    if (this.metaElements[k]) this.meta[k] = $(this.metaElements[k]).text()
-  })
-  this.content = this.toMd.turndown( $(this.contentElements.join(',')).html() )
+HtmlToMarkdown.prototype.loadUrl = fp.loadUrl
+
+HtmlToMarkdown.prototype.init = async function() {
+  const s = new Sema(1)
+  for (let url of this.subLinks) {
+    await s.acquire()
+    url = new URL(url, this.url).toString()
+    let stream = await this.loadUrl(url)
+    let doc = await this.getConverter(this.opts.converter || this.opts.c || 'html', stream, Object.assign({}, this.opts, {sourceUrl: url}))
+    await doc.init()
+    doc.convert()
+    this.subTexts.push(doc)
+    s.release()
+  }
+  return this
+}
+
+HtmlToMarkdown.prototype.prepareContent = function() {
+  if (this.subTexts.length) {
+    this.content = this.subTexts.map(doc => doc.content).join("\n\n* * *\n\n")
+  }
+  else {
+    this.content = this.toMd.turndown( this.$(this.opts.contentElements.join(', ')).html() )
+  }
+  return this
 }
 
 module.exports = HtmlToMarkdown
